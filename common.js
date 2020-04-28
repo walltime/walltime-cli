3a53b6dae91041591912088a5eb3b73e5935555b
Felipe Micaroni Lalli <micaroni@gmail.com>
2020-04-28 19:36:19 -0300

    Removing unused lib AWS.
    We make the request to AWS by http directly.

--- common.js
+++ common.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const AWS = require('aws-sdk');
 const crypto = require('crypto');
 const Message = require('bitcoinjs-message');
 const btc = require('bitcore-lib');
